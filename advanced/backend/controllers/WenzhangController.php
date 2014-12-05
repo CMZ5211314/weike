ab29389b758021980224e16145016900486c3d56
cmz <790330816@qq.com>
2014-12-05 16:35:08 +0800

    add

--- advanced/backend/controllers/WenzhangController.php
+++ advanced/backend/controllers/WenzhangController.php
@@ -1,4 +1,11 @@
 <?php
+namespace backend\controllers;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use common\models\LoginForm;
+use yii\filters\VerbFilter;
+
 class WenzhangController extends Controller{
 	public function actionGuanli(){
 		return $this->renderPartial('guanli');
