c504fc5b14ef9c30f83a30498ab412f73d69d74b
liujun <liujun@qq.com>
2014-12-05 10:32:09 +0800

    daa

---
+++ advanced/backend/controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+namespace backend\controllers;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use common\models\LoginForm;
+use yii\filters\VerbFilter;
+
+class IndexController extends Controller{
+	public function actionIndex(){
+		return $this->renderPartial('login');
+	}
+	public function actionMain(){
+		return $this->renderPartial('index');
+	}
+	public function actionCenter(){
+		return $this->renderPartial('center');
+	}
+	public function actionConfig(){
+		return $this->renderPartial('config');
+	}
+}
+?>
