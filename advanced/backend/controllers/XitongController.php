558bdad09ec661f065fa6508d769bc6305e49c5e
leiyaxin <908777566@qq.com>
2014-12-05 17:04:42 +0800

    add

--- advanced/backend/controllers/XitongController.php
+++ advanced/backend/controllers/XitongController.php
@@ -10,5 +10,17 @@ class XitongController extends Controller{
 	public function actionPeizhi(){
 		return $this->renderPartial('peizhi');
 	}
+	public function actionMoxing(){
+		return $this->renderPartial('moxing');
+	}
+	public function actionHuiyuan(){
+		return $this->renderPartial('huiyuan');
+	}
+	public function actionZidingyi(){
+		return $this->renderPartial('zidingyi');
+	}
+	public function actionHuilu(){
+		return $this->renderPartial('huilu');
+	}
 }
 ?>
