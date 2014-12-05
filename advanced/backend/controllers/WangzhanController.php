18c0139dfb2b7def3175c50dae04b874c3d8226a
leiyaxin <908777566@qq.com>
2014-12-05 15:20:19 +0800

    up

--- advanced/backend/controllers/WangzhanController.php
+++ advanced/backend/controllers/WangzhanController.php
@@ -1,4 +1,10 @@
 <?php
+namespace backend\controllers;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use common\models\LoginForm;
+use yii\filters\VerbFilter;
 class WangzhanController extends Controller{
 	public function actionGuanggao(){
 		return $this->renderPartial('guanggao');
