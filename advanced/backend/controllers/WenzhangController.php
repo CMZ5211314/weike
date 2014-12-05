1d5e1830cf189b4fc0bd4fc9879bfae7c82bc6d5
cmz <790330816@qq.com>
2014-12-05 16:33:49 +0800

    add

--- advanced/backend/controllers/WenzhangController.php
+++ advanced/backend/controllers/WenzhangController.php
@@ -1,14 +1,7 @@
 <?php
-namespace backend\controllers;
-use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
-use common\models\LoginForm;
-use yii\filters\VerbFilter;
-
 class WenzhangController extends Controller{
-	public function actionIndex(){
-		return $this->renderPartial('article');
+	public function actionGuanli(){
+		return $this->renderPartial('guanli');
 	}
 }
 ?>
