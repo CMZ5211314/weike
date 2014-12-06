c413007cafad9e1aa4225490520996e98440d85e
zhangxiaoyun <18311171705@163.com>
2014-12-06 09:28:15 +0800

    sd

--- advanced/backend/controllers/IndexController.php
+++ advanced/backend/controllers/IndexController.php
@@ -3,9 +3,10 @@ namespace backend\controllers;
 
 class IndexController extends \yii\web\Controller
 {
+    /*
     public function actionIndex(){
 		return $this->renderPartial('login');
-	}
+	}*/
 	public function actionMain(){
 		return $this->renderPartial('index');
 	}
